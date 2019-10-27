212b14d7940c14e772d30cb1c542642cad45cab1
Allan Stewart <allan@Jacobs-MacBook-Pro.local>
2019-10-26 21:18:08 -0600

    Check for valid configuration

--- src/SubscriptionClientWrapper.cs
+++ src/SubscriptionClientWrapper.cs
@@ -18,13 +18,30 @@ namespace EL.ServiceBus
 
         public SubscriptionClientWrapper(ISubscriptionConfig config)
         {
+            if (string.IsNullOrEmpty(config.ConnectionString))
+            {
+                throw new ArgumentException($"Invalid ConnectionString in ISubscriptionConfig", nameof(config.ConnectionString));
+            }
+            if (string.IsNullOrEmpty(config.TopicName))
+            {
+                throw new ArgumentException($"Invalid TopicName in ISubscriptionConfig", nameof(config.TopicName));
+            }
+            if (string.IsNullOrEmpty(config.SubscriptionName))
+            {
+                throw new ArgumentException($"Invalid SubscriptionName in ISubscriptionConfig", nameof(config.SubscriptionName));
+            }
+            if (config.MaxConcurrentMessages < 1)
+            {
+                throw new ArgumentException($"ISubscriptionConfig.MaxConcurrentMessages must be greater than zero.", nameof(config.MaxConcurrentMessages));
+            }
+
             client = new SubscriptionClient(config.ConnectionString, config.TopicName, config.SubscriptionName);
             this.config = config;
         }
 
         public void Subscribe(Action<string> messageHandler, Action<ExceptionReceivedEventArgs> exceptionHandler)
         {
-            var options = new MessageHandlerOptions((ExceptionReceivedEventArgs exceptionReceivedEventArgs) => {
+            var options = new MessageHandlerOptions(exceptionReceivedEventArgs => {
                 exceptionHandler(exceptionReceivedEventArgs);
                 return Task.CompletedTask;
             })
@@ -32,10 +49,10 @@ namespace EL.ServiceBus
                 MaxConcurrentCalls = config.MaxConcurrentMessages,
                 AutoComplete = false
             };
-            client.RegisterMessageHandler(async (Message message, CancellationToken token) => {
+            client.RegisterMessageHandler(async (message, cancellationToken) => {
                 var serializedMessage = Encoding.UTF8.GetString(message.Body);
                 messageHandler(serializedMessage);
-                if (!token.IsCancellationRequested)
+                if (!cancellationToken.IsCancellationRequested)
                 {
                     await client.CompleteAsync(message.SystemProperties.LockToken);
                 }
